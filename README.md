e994e11f5129df33fd9e2817685a1b5e8cf8c861
rjarrin <jarrin.ricardo@gmail.com>
2024-02-16 14:29:24 -0500

    Completed RPS js project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # js_rockpaperscissors
+
+A simple rock-paper-scissors game using JavaScript
+Project from https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
---
+++ javascript/main.js
@@ -0,0 +1,80 @@
+function playGame(score) {
+    
+    /* Ask player to select their choice */
+    let hand = playerSelection();
+    console.log("Player selected \"" + hand + "\".");
+
+    /* Have the computer select a random option */
+    let dealer = getComputerChoice(3);
+    console.log("Computer selected \"" + dealer + "\".");
+
+    /* Winner declaration */
+    let check = compareChoices(hand, dealer);
+
+    if (check == 0) {
+        score.userScore = score.userScore + 1;
+    } else if (check == 1) {
+        score.computerScore = score.computerScore + 1;
+    }
+}
+
+function playerSelection() {
+    let choice = prompt("Choose a hand option (rock, paper, scissors): ");
+    choice = choice.toLowerCase();
+
+    while (choice != "rock" && choice != "paper" && choice !== "scissors") {
+        choice = prompt("Invalid input. Try again: ");
+        choice = choice.toLowerCase()
+    }
+    return choice;
+}
+
+function getComputerChoice(choice) {
+    let response = Math.floor(Math.random() * choice);
+    if (response == 0) {
+        return "rock";
+    }
+    else if (response == 1) {
+        return "paper";
+    }
+    else if (response == 2) {
+        return "scissors";
+    }
+    else {
+        return "getComputerChoice: Error";
+    }
+}
+
+function compareChoices(hand, dealer) {
+    if ((hand == "rock" && dealer == "scissors") 
+    || (hand == "paper" && dealer == "rock") 
+    || (hand == "scissors" && dealer == "paper")) {
+        console.log("You Win! " +hand+ " beats " +dealer+ ".");
+        return 0;
+    }
+    else if ((dealer == "rock" && hand == "scissors") 
+    || (dealer == "paper" && hand == "rock") 
+    || (dealer == "scissors" && hand == "paper")) {
+        console.log("You Lose! " +dealer+ " beats " +hand+ ".");
+        return 1;
+    }
+    else {
+        console.log("Tied Game.");
+        return 2;
+    }
+}
+
+let score = {
+    userScore : 0,
+    computerScore : 0
+}
+
+let games = 5;
+
+/* Introduce the player to the game */
+console.log("Welcome to this Rock-Paper-Scissors Simulator");
+for (let i = 0; i < games; i++) {
+    playGame(score);
+}
+
+console.log("Final Score (P:C) is "+score.userScore+":"+score.computerScore);
